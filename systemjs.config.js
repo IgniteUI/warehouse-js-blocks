7e4e9e1e1c3b75a9a9fd5c7672d761fec60d303b
Konstantin Dinev <kdinev@mail.bw.edu>
2016-10-28 17:55:30 +0300

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': './node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -29,18 +29,18 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: 'main.js',
+        main: './main.js',
         defaultExtension: 'js'
       },
       rxjs: {
         defaultExtension: 'js'
       },
       'angular-in-memory-web-api': {
-        main: 'index.js',
+        main: './index.js',
         defaultExtension: 'js'
       },
       'zero-blocks': {
-        main: 'main.js',
+        main: './main.js',
         defaultExtension: 'js'
       }
     }
