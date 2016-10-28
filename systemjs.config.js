2c96489bb89567a66ccda496df48c2a20cda8247
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2016-10-28 18:18:37 +0300

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': './node_modules/'
+      'npm:': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
