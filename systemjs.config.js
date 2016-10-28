29af89e70f0fbe687c38e332d7d5efc118bf9cd6
Konstantin Dinev <kdinev@mail.bw.edu>
2016-10-28 17:44:36 +0300

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'https://rawgit.com/IgniteUI/warehouse-js-blocks/master/node_modules/'
+      'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -29,18 +29,18 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: 'https://rawgit.com/IgniteUI/warehouse-js-blocks/master/main.js',
+        main: 'main.js',
         defaultExtension: 'js'
       },
       rxjs: {
         defaultExtension: 'js'
       },
       'angular-in-memory-web-api': {
-        main: 'https://rawgit.com/IgniteUI/warehouse-js-blocks/master/index.js',
+        main: 'index.js',
         defaultExtension: 'js'
       },
       'zero-blocks': {
-        main: 'https://rawgit.com/IgniteUI/warehouse-js-blocks/master/main.js',
+        main: 'main.js',
         defaultExtension: 'js'
       }
     }
