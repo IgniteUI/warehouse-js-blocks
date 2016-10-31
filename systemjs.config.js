7fde689df7989aec4165fd631d6cc556ebfe63f5
Radoslav Karaivanov <RKaraivanov@infragistics.com>
2016-10-31 16:08:26 +0200

    Prep for GH pages

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -29,8 +29,8 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
-        defaultExtension: 'js'
+        main: './main.ts',
+        defaultExtension: 'ts'
       },
       rxjs: {
         defaultExtension: 'js'
@@ -43,6 +43,10 @@
         main: './main.js',
         defaultExtension: 'js'
       }
+    },
+    transpiler: 'typescript',
+    typescriptOptions: {
+      emitDecoratorMetadata: true
     }
   });
 })(this);
