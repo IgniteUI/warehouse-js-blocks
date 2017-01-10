5d5d1cc95c4df06b4ba2be4ef6e4e7114973312f
Radoslav Karaivanov <RKaraivanov@infragistics.com>
2017-01-10 12:32:23 +0200

    More fixes for gh-pages

--- systemjs.config.js
+++ systemjs.config.js
@@ -16,7 +16,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'app',
+      app: 'app/',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
