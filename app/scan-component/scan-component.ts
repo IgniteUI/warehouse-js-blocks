7703e992e986c57866a8a71f3b1a163a751a9416
Radko Kolev <rkolev@infragistics.com>
2018-02-26 16:30:37 +0200

    Removing old warehouse app code

--- app/scan-component/scan-component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'scanner',
-    moduleId: module.id,
-    templateUrl: 'scan-component.html'
-})
-export class ScanComponent {
-}
